9f1838c5f27bf1aa90355a334b38c3c3a7ffdfdd
abdurrahman-abdulhakeem <abdurrahmanola21@gmail.com>
2024-09-01 14:08:27 +0100

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://abdurrahman.ng', // Replace with your website URL
+    generateRobotsTxt: true, // Generate robots.txt file
+    sitemapSize: 7000, // Split sitemap into multiple files if it exceeds this size
+  };
+  
--- next-sitemap.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    siteUrl: 'https://abdurrahman.ng',
-    generateRobotsTxt: true,
-  };
-  
